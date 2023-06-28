9f99e1453b94604115604fe79fe16964f5d79b3d
Mark Wilkinson <markw@illuminae.com>
2023-06-28 09:30:18 +0200

    added the definitively mapped taxons

---
+++ scripts/create-theme-keyword-fdp.rb
@@ -0,0 +1,25 @@
+# Objective:
+# dcat:theme <http://purl.org/ejp-rd/vocabulary/VPDiscoverable>, <http://semanticscience.org/resource/SIO_000614> ;
+#
+# dcat:keyword "Becker Muscular Dystrophy", "Clinical data", "Duchenne Muscular Dystrophy", "Genetic data", "PROMs", "PROs";
+
+require 'csv'
+
+theme = "dcat:theme "
+keywords = "dcat:keyword "
+CSV.foreach("./output/species-taxid.csv") do |line|
+    kw = line[0]
+    tid = line[1]
+    next unless kw && tid
+    theme += "<#{tid}>, "
+    keywords += "\"#{kw}\", "
+end
+
+theme.gsub!(/\,\s+$/, "")
+keywords.gsub!(/\,\s+$/, "")
+theme += " ;"
+keywords += " ;"
+
+puts theme
+puts; puts; puts
+puts keywords
