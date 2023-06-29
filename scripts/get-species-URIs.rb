eb570e739de5c4dfe8e7f3d0c098cc751aa3d99c
Mark Wilkinson <markw@illuminae.com>
2023-06-29 02:54:43 +0200

    more transformation scripts to load fdp

--- scripts/get-species-URIs.rb
+++ scripts/get-species-URIs.rb
@@ -4,24 +4,37 @@ require 'csv' # can't handle non-UTF8 characters
 
 `echo "" > 'not-found.txt'`
 `echo "" > 'multiple-found.txt'`
-`echo "" > './output/species-taxid.csv'`
+`echo "" > './output/species-taxid2.csv'`
 
-CSV.foreach('./data/species-from-alberto.csv', headers: true, encoding: 'ISO8859-1') do |row|
-  species = row['Nombre aceptado']
+flag = 0
+CSV.foreach('./data/species-from-alberto2.csv', headers: false, encoding: 'UTF-8') do |row|
+  #  species = row['Nombre aceptado']
+  species = row[0]
   next unless species
+  species.strip!
+  next unless species
+  next unless species =~ /\s/ # must be binomial scientific name
+
   next if species.empty?
+
   begin
     species = species.encode('UTF-8')
   rescue Encoding::UndefinedConversionError
     abort "this didn't work - bad encoding"
   end
 
+
   # deal with non-ASCII characters that come from stupid MS Windows
   species.gsub!(/[[:space:]]/, '%20')
   species.gsub!(/\P{ASCII}/, '')
   species.gsub!(/%20$/, '')
   warn "https://api.gbif.org/v1/species?name=#{species}"
-  results = RestClient.get("https://api.gbif.org/v1/species?name=#{species}")
+  begin
+    results = RestClient.get("https://api.gbif.org/v1/species?name=#{species}")
+  rescue
+    results = RestClient.get("https://api.gbif.org/v1/species?name=#{species}")
+  end
+
   parsed = JSON.parse(results)
   # warn parsed
   species.gsub! /%20/, " "  # convert back from URL form
@@ -47,5 +60,5 @@ CSV.foreach('./data/species-from-alberto.csv', headers: true, encoding: 'ISO8859
   gbif = accepted_result['taxonID'] # gbif:5370217
   # https://www.gbif.org/species/5370217
   gbif.gsub!(/gbif:/, 'https://www.gbif.org/species/')
-  File.open('./output/species-taxid.csv', 'a') { |f| f.puts "#{species},#{gbif}" }
+  File.open('./output/species-taxid2.csv', 'a') { |f| f.puts "#{species},#{gbif}" }
 end
