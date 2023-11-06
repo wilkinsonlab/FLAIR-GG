97307e5e0223bae65b36247b988249b96a750ce8
Mark Wilkinson <markw@illuminae.com>
2023-11-06 13:38:53 +0100

    wrong filename

--- TransformationPipeline/FLAIR-box-daemon/transform.rb
+++ TransformationPipeline/FLAIR-box-daemon/transform.rb
@@ -1,11 +1,11 @@
-require 'sinatra'
-require 'rest-client'
-require './http_utils'
-require 'open3'
+require "sinatra"
+require "rest-client"
+require "./http_utils"
+require "open3"
 
 include HTTPUtils
 
-get '/' do
+get "/" do
   update
   yarrrml_substitute
   execute
@@ -16,23 +16,24 @@ get '/' do
 end
 
 def update
-  warn 'first open3 git pull'
-  o, e, _s = Open3.capture3('cd FLAIR-GG && git pull')
+  warn "first open3 git pull"
+  o, e, _s = Open3.capture3("cd FLAIR-GG && git pull")
+  warn `pwd`
+  warn `ls`
   warn "second open3 copy yarrrml #{o}  #{e}"
-  o, e, _s = Open3.capture3('cp -rf ./SemanticModel/YARRRML/*.yaml  /data') # CDE V2
+  o, e, _s = Open3.capture3("cp -rf ./SemanticModel/YARRRML/*.yaml  /data") # CDE V2
   warn "second open3 complete #{o} #{e}"
 end
 
-
 def yarrrml_substitute
   warn "starting yarrrml substitution"
-  baseURI = ENV.fetch('baseURI', 'http://example.org/')
-  baseURI = 'http://example.org/' if baseURI.empty?
-  template_list = Dir['/conf/*.pre-yaml']
-  template_list.each do |t|  
+  baseURI = ENV.fetch("baseURI", "http://example.org/")
+  baseURI = "http://example.org/" if baseURI.empty?
+  template_list = Dir["/conf/*.pre-yaml"]
+  template_list.each do |t|
     content = File.read(t)
-    content.gsub!('|||baseURI|||', baseURI)
-    newfile = t.gsub!(/\.pre-yaml/, 'yaml')
+    content.gsub!("|||baseURI|||", baseURI)
+    newfile = t.gsub!(".pre-yaml", "yaml")
     warn "writing #{newfile}"
     f = File.open(newfile, "w")
     f.puts content
@@ -44,9 +45,9 @@ end
 def execute
   warn "executing transform"
   purge_nq
-  @datatype_list = Dir['/data/*.csv']
+  @datatype_list = Dir["/data/*.csv"]
   @datatype_list.each do |d|
-    datatype = d.match(%r{.+/([^.]+)\.csv})[1] 
+    datatype = d.match(%r{.+/([^.]+)\.csv})[1]
     next unless datatype
 
     _resp = RestClient.get("http://yarrrml-rdfizer:4567/#{datatype}")
@@ -55,43 +56,43 @@ def execute
 end
 
 def load_flair
-  files = Dir['/data/triples/*.nq']
-  concatenated = ''
+  files = Dir["/data/triples/*.nq"]
+  concatenated = ""
   files.each do |f|
     warn "Processing file #{f}"
-    datatype = d.match(%r{.+/([^.]+)\.nq})[1] 
+    datatype = f.match(%r{.+/([^.]+)\.nq})[1]
     content = File.read(f)
-    write_to_graphdb(content, datatype)   
+    write_to_graphdb(content, datatype)
     warn "wrote #{datatype}"
   end
 end
 
 def write_to_graphdb(concatenated, reponame)
-  user = ENV.fetch('GraphDB_User', nil)
-  pass = ENV.fetch('GraphDB_Pass', nil)
-  network = ENV['networkname'] || 'graphdb'
-#  reponame = ENV.fetch('GRAPHDB_REPONAME')
+  user = ENV.fetch("GraphDB_User", nil)
+  pass = ENV.fetch("GraphDB_Pass", nil)
+  network = ENV["networkname"] || "graphdb"
+  #  reponame = ENV.fetch('GRAPHDB_REPONAME')
   url = "http://#{network}:7200/repositories/#{reponame}/statements"
   #  headers = { content_type: 'application/n-triples' }
-  headers = { content_type: 'application/n-quads' }
+  headers = { content_type: "application/n-quads" }
   HTTPUtils.put(url, headers, concatenated, user, pass)
 end
 
 def purge_nq
-  File.delete('/data/triples/*.nq')
+  File.delete("/data/triples/*.nq")
 rescue StandardError
-  warn 'Deleting the exisiting .nq files failed!'
+  warn "Deleting the exisiting .nq files failed!"
 ensure
-  warn 'looks like it is already clean in here!'
+  warn "looks like it is already clean in here!"
 end
 
 def metadata_update
-  return if ENV['DIST_RECORDID'].nil? || ENV['DATASET_RECORDID'].nil? || ENV['DATA_SPARQL_ENDPOINT'].nil?
-  return if ENV['DIST_RECORDID'].empty? || ENV['DATASET_RECORDID'].empty? || ENV['DATA_SPARQL_ENDPOINT'].empty?
+  return if ENV["DIST_RECORDID"].nil? || ENV["DATASET_RECORDID"].nil? || ENV["DATA_SPARQL_ENDPOINT"].nil?
+  return if ENV["DIST_RECORDID"].empty? || ENV["DATASET_RECORDID"].empty? || ENV["DATA_SPARQL_ENDPOINT"].empty?
 
-  warn 'calling metadata updater image'
+  warn "calling metadata updater image"
   begin
-    resp = RestClient.get('http://updater:4567/update')
+    resp = RestClient.get("http://updater:4567/update")
   rescue StandardError
     warn "\n\n\ncall to http://updater:4567/update FAILED"
     warn resp
@@ -100,6 +101,6 @@ def metadata_update
 end
 
 def cleanup
-  warn 'closing cleanup open3'
-  _o, _s = Open3.capture2('rm -rf /data/triplesstats.csv')
+  warn "closing cleanup open3"
+  _o, _s = Open3.capture2("rm -rf /data/triplesstats.csv")
 end
