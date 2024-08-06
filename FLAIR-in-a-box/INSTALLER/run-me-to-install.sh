5c2e7b43fd10fb60eb88b30fc10cb3f84747e0a6
Mark Wilkinson <markw@illuminae.com>
2024-08-06 15:36:25 +0200

    longer sleep

--- FLAIR-in-a-box/INSTALLER/run-me-to-install.sh
+++ FLAIR-in-a-box/INSTALLER/run-me-to-install.sh
@@ -130,7 +130,7 @@ cp docker-compose-template.yml "docker-compose-${P}.yml"
 sed -i s/{PREFIX}/${P}/ "docker-compose-${P}.yml"
 
 docker-compose -f "docker-compose-${P}.yml" up --build -d
-sleep 13
+sleep 60
 echo ""
 echo -e "${GREEN}Setting up FAIR Data Point client and server${NC}"
 echo ""
@@ -152,7 +152,7 @@ sed -i s%{GUID}%$uri% "./fdp/application-${P}.yml"
 docker-compose -f "docker-compose-${P}.yml" up --build -d
 
 
-sleep 15
+sleep 60
 
 echo ""
 echo -e "${GREEN}Creating a production server folder in ${NC} ./${P}-ready-to-go/"
