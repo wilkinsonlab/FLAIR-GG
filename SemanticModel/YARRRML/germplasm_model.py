c2ee28bd6e5c241ca99c1478d755cd92aa6a0c1c
Acb897 <camara.alber@gmail.com>
2024-11-06 19:18:45 +0100

    Updated germplassm model YARRRML generator, created CSV template

--- SemanticModel/YARRRML/germplasm_model.py
+++ SemanticModel/YARRRML/germplasm_model.py
@@ -6,10 +6,12 @@ data = {
     "rdf" : "http://www.w3.org/1999/02/22-rdf-syntax-ns#" ,
     "rdfs" : "http://www.w3.org/2000/01/rdf-schema#" ,
     "sio" : "http://semanticscience.org/resource/" ,
-    "geo" : "http://www.w3.org/2003/01/geo/wgs84_pos#" , 
     "efo" : "https://www.ebi.ac.uk/efo/" ,
     "dwc" : "http://rs.tdwg.org/dwc/terms/" ,
     "prov" :  "http://www.w3.org/ns/prov#" ,
+    "edam" : "http://edamontology.org/",
+    "fao": "https://w3id.org/fao-ipgr/multi-passport-descriptor.owl#",
+    "xsd" : "http://www.w3.org/2001/XMLSchema#",
     "this" : "http://my_example.com/"},
    
 
@@ -17,31 +19,81 @@ data = {
     ["this:$(bgvid)_$(uniqid)#Collection","sio:SIO_000229","this:$(uniqid)#Germplasm","iri"],
     ["this:$(uniqid)#Germplasm", "prov:wasgeneratedby", "this:$(bgvid)_$(uniqid)#Collection", "iri"],
     ["this:$(uniqid)#Germplasm", "sio:SIO_000671", "this:$(uniqid)#identifier", "iri"],
-    ["this:$(uniqid)#Germplasm", "prov:wasUsedBy", "this:$(uniqid)#Plant_identification_process", "iri"],
-    ["this:$(uniqid)#Germplasm", "sio:SIO_000231", "this:$(uniqid)#Plant_identification_process", "iri"],
-    ["this:$(uniqid)#Plant_identification_process", "sio:SIO_000229", "this:$(uniqid)#Plant_identification", "iri"],
-    ["this:$(uniqid)#Germplasm", "prov:wasUsedBy", "this:$(uniqid)#Endangerment_assesment_process", "iri"],
-    ["this:$(uniqid)#Germplasm", "sio:SIO_000231", "this:$(uniqid)#Endangerment_assesment_process", "iri"],
-    ["this:$(uniqid)#Endangerment_assesment_process", "sio:SIO_000229", "this:$(uniqid)#Endangerment_category", "iri"],
+    ["this:$(uniqid)#Germplasm", "sio:SIO_000671", "this:$(uniqid)#Accession_number", "iri"],
+    ["this:$(uniqid)#Germplasm", "sio:SIO_000671", "this:$(uniqid)#National_catalogue_code", "iri"],
+    ["this:$(uniqid)#Germplasm", "prov:wasUsedBy", "this:$(uniqid)#Plant_determination_process_WFO", "iri"],
+    ["this:$(uniqid)#Germplasm", "prov:wasUsedBy", "this:$(uniqid)#Plant_determination_process_lista_patron", "iri"],
+    ["this:$(uniqid)#Germplasm", "sio:SIO_000231", "this:$(uniqid)#Plant_determination_process_WFO", "iri"],
+    ["this:$(uniqid)#Germplasm", "sio:SIO_000231", "this:$(uniqid)#Plant_determination_process_lista_patron", "iri"],
+    ["this:$(uniqid)#Plant_determination_process_WFO", "sio:SIO_000229", "this:$(uniqid)#Plant_identification_WFO", "iri"],
+    ["this:$(uniqid)#Plant_determination_process_lista_patron", "sio:SIO_000229", "this:$(uniqid)#Plant_identification_lista_patron", "iri"],
+    ["this:$(uniqid)#Germplasm", "prov:wasUsedBy", "this:$(uniqid)#Endangerment_assesment_process_LESPRE", "iri"],
+    ["this:$(uniqid)#Germplasm", "sio:SIO_000231", "this:$(uniqid)#Endangerment_assesment_process_IUCN", "iri"],
+    ["this:$(uniqid)#Germplasm", "prov:wasUsedBy", "this:$(uniqid)#Endangerment_assesment_process_LESPRE", "iri"],
+    ["this:$(uniqid)#Germplasm", "sio:SIO_000231", "this:$(uniqid)#Endangerment_assesment_process_IUCN", "iri"],
+    ["this:$(uniqid)#Endangerment_assesment_process", "sio:SIO_000229", "this:$(uniqid)#Endangerment_category_LESPRE", "iri"],
+    ["this:$(uniqid)#Endangerment_assesment_process", "sio:SIO_000229", "this:$(uniqid)#Endangerment_category_IUCN", "iri"],
       
 
     ["this:$(bgvid)_$(uniqid)#Collection","rdf:type","prov:Activity","iri"],
     ["this:$(bgvid)_$(uniqid)#Collection","rdf:type","sio:SIO_1049","iri"],
-    ["this:$(bgvid)_$(uniqid)#Collection","rdfs:label","$(comment)","xsd:string"],
+    ["this:$(bgvid)_$(uniqid)#Collection","rdfs:comment","$(collection_comment)","xsd:string"],
     ["this:$(uniqid)#Germplasm", "rdf:type", "sio:SIO_000000", "iri"],
     ["this:$(uniqid)#Germplasm", "rdf:type", "efo:EFO_007059", "iri"],
     ["this:$(uniqid)#Germplasm", "rdf:type", "prov:Entity", "iri"],
+
     ["this:$(uniqid)#identifier","sio:SIO_000300","$(bgvid)","xsd:string"],
-    ["this:$(uniqid)#Plant_identification_process", "rdf:type", "prov:activity", "iri"],
-    ["this:$(uniqid)#Plant_identification_process", "rdf:type", "sio:SIO_000006", "iri"],
-    ["this:$(uniqid)#Plant_identification", "dwc:vernacularName", "sio:SIO_000116", "iri"],
-    ["this:$(uniqid)#Plant_identification", "rdf:type", "$(Vernacular_name)", "iri"],
-    ["this:$(uniqid)#Plant_identification", "dwc:sientificName", "$(Scientific_name)", "iri"],
-    ["this:$(uniqid)#Plant_identification", "dwc:sientificNameAuthorship", "$(Vernacular_name_authorship)", "iri"],
-    ["this:$(uniqid)#Endangerment_assesment_process", "rdf:type", "sio:SIO_000006", "iri"],
-    ["this:$(uniqid)#Endangerment_assesment_process", "rdf:type", "prov:activity", "iri"],
-    ["this:$(uniqid)#Endangerment_category", "rdf:type", "", "iri"],
-    ["this:$(uniqid)#Endangerment_category", "sio:SIO_000300", "", "iri"],
+    ["this:$(uniqid)#Accession_number","sio:SIO_000300","$(accession_number)","xsd:string"],
+    ["this:$(uniqid)#National_catalogue_code","sio:SIO_000300","$(national_catalogue_code)","xsd:string"],
+
+    ["this:$(uniqid)#identifier","rdf:type","SIO:000115","iri"],
+    ["this:$(uniqid)#Accession_number","rdf:type","SIO:000115","iri"],
+    ["this:$(uniqid)#National_catalogue_code","rdf:type","SIO:000115","iri"],
+
+    
+    ["this:$(uniqid)#Accession_number","rdf:type","fao:CO_020:0000089","iri"],
+    ["this:$(uniqid)#National_catalogue_code","rdf:type","fao:national_catalogue_code","iri"],
+
+    ["this:$(uniqid)#Plant_determination_process_WFO", "rdf:type", "prov:activity", "iri"],
+    ["this:$(uniqid)#Plant_determination_process_WFO", "rdf:type", "sio:SIO_000006", "iri"],
+    ["this:$(uniqid)#Plant_determination_process_lista_patron", "rdf:type", "prov:activity", "iri"],
+    ["this:$(uniqid)#Plant_determination_process_lista_patron", "rdf:type", "sio:SIO_000006", "iri"],
+
+    ["this:$(uniqid)#Plant_identification_WFO", "dwc:vernacularName", "$(WFO_vernacular_name)", "iri"],
+    ["this:$(uniqid)#Plant_identification_WFO", "rdf:type", "dwc:Taxon", "iri"],
+    ["this:$(uniqid)#Plant_identification_WFO", "dwc:sientificName", "$(WFO_scientific_name)", "iri"],
+    ["this:$(uniqid)#Plant_identification_WFO", "dwc:sientificNameAuthorship", "$(WFO_scientific_name_authorship)", "iri"],
+
+    ["this:$(uniqid)#Plant_identification_lista_patron", "dwc:vernacularName", "$(lista_patron_vernacular_name)", "iri"],
+    ["this:$(uniqid)#Plant_identification_lista_patron", "rdf:type", "dwc:Taxon", "iri"],
+    ["this:$(uniqid)#Plant_identification_lista_patron", "dwc:sientificName", "$(lista_patron_scientific_name)", "iri"],
+    ["this:$(uniqid)#Plant_identification_lista_patron", "dwc:sientificNameAuthorship", "$(lista_patron_scientific_name_authorship)", "iri"],
+
+    ["this:$(uniqid)#Plant_identification_lista_patron", "sio:SIO_000671", "this:$(uniqid)#Lista_patron_identifier", "iri"],
+    ["this:$(uniqid)#Plant_identification_WFO", "sio:SIO_000671", "this:$(uniqid)#WFO_identifier", "iri"],
+
+    ["this:$(uniqid)#Lista_patron_identifier", "rdf:type", "sio:SIO_000115", "iri"],
+    ["this:$(uniqid)#WFO_identifier", "rdf:type", "sio:SIO_000115", "iri"],
+    ["this:$(uniqid)#Lista_patron_identifier", "sio:SIO_000300", "$(lista_patron_identifier)", "xsd:string"],
+    ["this:$(uniqid)#WFO_identifier", "sio:SIO_000300", "$(WFO_identifier)", "xsd:string"],
+
+    ["this:$(uniqid)#Plant_identification_lista_patron", "sio:SIO_000008", "this:$(uniqid)#Determination_status_lista_patron", "iri"],
+    ["this:$(uniqid)#Plant_identification_WFO", "sio:SIO_000008", "this:$(uniqid)#Determination_status_WFO", "iri"],
+
+    ["this:$(uniqid)#Determination_status_lista_patron", "rdf:type", "fao:determination_status", "iri"]
+    ["this:$(uniqid)#Determination_status_WFO", "rdf:type", "fao:determination_status", "iri"]
+    ["this:$(uniqid)#Determination_status_lista_patron", "sio:SIO_000300", "$(lista_patron_determination_status)", "xsd:string"]
+    ["this:$(uniqid)#Determination_status_WFO", "sio:SIO_000300", "$(WFO_determination_status)", "xsd:string"]
+
+    ["this:$(uniqid)#Endangerment_assesment_process_LESPRE", "rdf:type", "sio:SIO_000006", "iri"],
+    ["this:$(uniqid)#Endangerment_assesment_process_LESPRE", "rdf:type", "prov:activity", "iri"],
+    ["this:$(uniqid)#Endangerment_assesment_process_IUCN", "rdf:type", "sio:SIO_000006", "iri"],
+    ["this:$(uniqid)#Endangerment_assesment_process_IUCN", "rdf:type", "prov:activity", "iri"],
+ 
+    ["this:$(uniqid)#Endangerment_category_LESPRE", "rdf:type", "fao:endangerment_category", "iri"],
+    ["this:$(uniqid)#Endangerment_category_LESPRE", "sio:SIO_000300", "$(lespre_endangerment_category)", "iri"],
+    ["this:$(uniqid)#Endangerment_category_IUCN", "rdf:type", "fao:endangerment_category", "iri"],
+    ["this:$(uniqid)#Endangerment_category_IUCN", "sio:SIO_000300", "$(iucn_endangerment_category)", "iri"],
 
 
 
