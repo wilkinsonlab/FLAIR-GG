65ddf5589609adbf153a9824180b162d85f3b73a
Mark Wilkinson <markw@illuminae.com>
2024-11-30 17:15:51 +0100

    afsdas

--- VP/wrapped_services/app/controllers/routes/driada/routes.rb
+++ VP/wrapped_services/app/controllers/routes/driada/routes.rb
@@ -20,6 +20,7 @@
     request.accept.each do |type|
       case type.to_s
       when "text/html"
+        # need to set views for this wrapped service.  This may not be sustainable...
         set :views, "app/controllers/views/driada"
         halt erb :species
       else
@@ -30,11 +31,12 @@
   end
 
   get "/wrapped-services/driada/interface/form" do
+    # need to set views for this wrapped service.  This may not be sustainable...
     set :views, "app/controllers/views/driada"
     halt erb :lookupform
   end
 
   get "/wrapped-services/driada/interface" do
-    redirect "https://wilkinsonlab.github.io/FLAIR-GG/VP/interfaces/wrapped_services_driada.yaml"
+    redirect "https://wilkinsonlab.github.io/FLAIR-GG/VP/interfaces/wrapped_services.yaml"
   end
 # end
