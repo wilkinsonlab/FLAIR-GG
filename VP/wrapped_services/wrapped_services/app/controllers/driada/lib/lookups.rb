586190308df3a2c952136725ff46b8db77eef146
Mark Wilkinson <markw@illuminae.com>
2024-11-30 20:50:12 +0100

    damn microsoft encoding not being utf8 by default!  DAMN THEM To HELL!

--- VP/wrapped_services/wrapped_services/app/controllers/driada/lib/lookups.rb
+++ VP/wrapped_services/wrapped_services/app/controllers/driada/lib/lookups.rb
@@ -2,7 +2,7 @@ def species_lookup(species:)
   warn `pwd`
   warn species
   # abort
-  table = CSV.parse(File.read(DRIADA), headers: true, col_sep: ";")
+  table = CSV.parse(File.read(DRIADA), headers: true, col_sep: ";", encoding: "UTF-8")
   table.each do |row|
     # warn row.inspect
     data = row.to_h
