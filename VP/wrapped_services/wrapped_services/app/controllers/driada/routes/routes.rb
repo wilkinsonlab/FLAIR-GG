3fbce652750833ea986e909ee3f76d994f900ac4
Mark Wilkinson <markw@illuminae.com>
2024-11-30 20:10:46 +0100

    update redirects and ontology

--- VP/wrapped_services/wrapped_services/app/controllers/driada/routes/routes.rb
+++ VP/wrapped_services/wrapped_services/app/controllers/driada/routes/routes.rb
@@ -5,16 +5,57 @@
 
   DRIADA = "./app/controllers/driada/rawdata/out.csv".freeze
 
-  get "/wrapped-services/driada" do
-    redirect "/wrapped-services/driada/interface"
+  get "/driada" do
+    # case type.to_s
+    # when "text/html"
+      redirect "https://vp.bgv.cbgp.upm.es/wrapped-services/driada/interface"
+      halt
+    # else
+    #   redirect "/wrapped-services/driada/interface/form"
+    #   halt
+    # end
   end
 
-  get "/wrapped-services/driada/lookup" do
+  get "/driada/" do
+    # case type.to_s
+    # when "text/html"
+      redirect "https://vp.bgv.cbgp.upm.es/wrapped-services/driada/interface"
+      halt
+    # else
+    #   redirect "/wrapped-services/driada/interface/form"
+    #   halt
+    # end
+  end
+
+  get "/driada/lookup" do
     unless params["species"]
-      redirect "/wrapped-services/driada/interface/form"
+      # one day, solve this problem in the lighttpd proxy!  grrrrr
+      redirect "https://vp.bgv.cbgp.upm.es/wrapped-services/driada/interface/form"
       halt
     end
-warn "PWD", `pwd`
+
+    species = params["species"] ? params["species"].strip : ""
+    @species = species
+    @resp = species_lookup(species: species) # "./lib/vp"
+
+    request.accept.each do |type|
+      case type.to_s
+      when "text/html"
+        # need to set views for this wrapped service.  This may not be sustainable...
+        halt erb :driada_species
+      else
+        content_type :json
+        halt @resp.to_json
+      end
+    end
+  end
+  get "/driada/lookup/" do
+    unless params["species"]
+      # one day, solve this problem in the lighttpd proxy!  grrrrr
+      redirect "https://vp.bgv.cbgp.upm.es/wrapped-services/driada/interface/form"
+      halt
+    end
+
     species = params["species"] ? params["species"].strip : ""
     @species = species
     @resp = species_lookup(species: species) # "./lib/vp"
@@ -31,12 +72,16 @@ warn "PWD", `pwd`
     end
   end
 
-  get "/wrapped-services/driada/interface/form" do
+  get "/driada/interface/form" do
+    # need to set views for this wrapped service.  This may not be sustainable...
+    halt erb :driada_lookupform
+  end
+  get "/driada/interface/form/" do
     # need to set views for this wrapped service.  This may not be sustainable...
     halt erb :driada_lookupform
   end
 
-  get "/wrapped-services/driada/interface" do
+  get "/driada/interface" do
     redirect "https://wilkinsonlab.github.io/FLAIR-GG/VP/interfaces/wrapped_services.yaml"
   end
 # end
